6faf1a6477cb3986a0fb45c8faa0cdb16e150062
Rafael Henrique Candido Da Silva <rafaelcandido@brq.com>
2017-05-05 19:29:42 -0300

    Settings

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,4 +2,19 @@ const BillingCycle = require('./billingCycle');
 
 BillingCycle.methods(['get','post','put','delete'])
 
+// Ao atualizar aparece o obj antigo, config para alterar este comportamento
+// Ativar as validações no update
+BillingCycle.updateOptions({new : true, runValidators : true});
+
+//Estabelecendo a rota de contador
+BillingCycle.route('count', function(req,res,next){
+	BillingCycle.count((err,value) => {
+		if(err){
+			res.status(500).json({errors: [err]})
+		}else{
+			res.json({value})
+		}
+	})
+})
+
 module.exports = BillingCycle;
